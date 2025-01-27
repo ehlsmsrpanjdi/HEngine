432052fb884a0af67ebc3bb7efcecbc0b8f4b271
성민 유 <usm9401@gmail.com>
2025-01-27 14:47:58 +0900

    d

--- HEngine/HDirect/HDirect/GraphicsEngine.h
+++ HEngine/HDirect/HDirect/GraphicsEngine.h
@@ -32,25 +32,26 @@ public:
 	static GraphicsEngine* get();
 
 private:
-	DeviceContext* m_imm_device_context;
+	DeviceContext* m_imm_device_context = nullptr;
 private:
-	ID3D11Device* m_d3d_device;
-	D3D_FEATURE_LEVEL m_feature_level;
+	ID3D11Device* m_d3d_device = nullptr;
+	D3D_FEATURE_LEVEL m_feature_level = D3D_FEATURE_LEVEL();
 private:
-	IDXGIDevice* m_dxgi_device;
-	IDXGIAdapter* m_dxgi_adapter;
-	IDXGIFactory* m_dxgi_factory;
-	ID3D11DeviceContext* m_imm_context;
+	IDXGIDevice* m_dxgi_device = nullptr;
+	IDXGIAdapter* m_dxgi_adapter = nullptr;
+	IDXGIFactory* m_dxgi_factory = nullptr;
+	ID3D11DeviceContext* m_imm_context = nullptr;
 
 private:
 	ID3DBlob* m_blob = nullptr;
-
-
-
 	ID3DBlob* m_vsblob = nullptr;
 	ID3DBlob* m_psblob = nullptr;
 	ID3D11VertexShader* m_vs = nullptr;
 	ID3D11PixelShader* m_ps = nullptr;
+
+
+	HWND m_hwnd = NULL;
+
 private:
 	friend class SwapChain;
 	friend class VertexBuffer;
