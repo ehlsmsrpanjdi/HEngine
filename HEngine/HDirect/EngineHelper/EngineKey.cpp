97ae25ca80cb472e9849730ec0d15f5d56990bc1
성민 유 <usm9401@gmail.com>
2025-02-01 19:37:57 +0900

    .,

---
+++ HEngine/HDirect/EngineHelper/EngineKey.cpp
@@ -0,0 +1,33 @@
+#include "EngineKey.h"
+
+std::map<int, Key> EngineKey::Keys;
+
+void EngineKey::KeyCheck() {
+	for (std::pair<const int, Key>& pa : Keys) {
+		if (GetAsyncKeyState(pa.first) & 0x8000) {
+			pa.second.On = true;
+			pa.second.Off = false;
+		}
+		else {
+			pa.second.On = false;
+			pa.second.Off = true;
+		}
+	}
+}
+
+void EngineKey::InitKey() {
+	Keys[VK_LEFT];
+	Keys[VK_RIGHT];
+	Keys[VK_UP];
+	Keys[VK_DOWN];
+	Keys['Z'];
+}
+
+bool EngineKey::IsInput(int _Key) {
+	if (Keys[_Key].On == true) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
---
+++ HEngine/HDirect/EngineHelper/EngineKey.h
@@ -0,0 +1,42 @@
+#pragma once
+#pragma once
+#include <Windows.h>
+#include <map>
+
+class Key;
+
+class EngineKey {
+public:
+	EngineKey() {
+	}
+	static void InitKey();
+	static void KeyCheck();
+	static bool IsInput(int _Key);
+
+private:
+	static std::map<int, Key> Keys;
+};
+
+class Key {
+public:
+	bool Down = false;
+	bool Up = false;
+	bool On = false;
+	bool Off = true;
+
+	void KeyPress() {
+		Down = true;
+		Up = false;
+		On = true;
+		Off = false;
+	}
+
+	void KeyUp() {
+		Down = false;
+		Up = false;
+		On = false;
+		Off = true;
+	}
+
+	int KeyType = -1;
+};
