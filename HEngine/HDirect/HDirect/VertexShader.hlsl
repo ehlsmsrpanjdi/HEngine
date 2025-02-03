f6b1e812e2e020a9913692b1342920ae2c90b857
성민 유 <usm9401@gmail.com>
2025-02-03 22:06:47 +0900

    asdef

--- HEngine/HDirect/HDirect/VertexShader.hlsl
+++ HEngine/HDirect/HDirect/VertexShader.hlsl
@@ -11,10 +11,22 @@ struct VS_OUTPUT
 };
 
 
+cbuffer constant : register(b0)
+{
+    row_major float4x4 m_world;
+    row_major float4x4 m_view;
+    row_major float4x4 m_proj;
+    unsigned int m_time;
+};
+
 VS_OUTPUT vsmain(VS_INPUT input)
 {
     VS_OUTPUT output = (VS_OUTPUT) 0;
 	
+    output.position = mul(input.position, m_world);
+    output.position = mul(input.position, m_view);
+    output.position = mul(input.position, m_proj);
+    
     output.position = input.position;
     output.color = input.color;
 
