6a7a8a7865f55873667b507afbb29fda511887c1
성민 유 <usm9401@gmail.com>
2025-04-10 16:12:48 +0900

    유성민 : 스켈레톤 가중이 입력 완료 및 디버깅 안함

--- HEngine/HHH/GameEngine/GameEngine.cpp
+++ HEngine/HHH/GameEngine/GameEngine.cpp
@@ -35,7 +35,7 @@ void GameEngine::Init(RECT _rc)
 	SetMainCamera("temp");
 
 	Actor* act = SpawnActor<Actor>().get();
-	act->SetMesh("HipHop");
+	act->SetMesh("hiphop");
 	act->SetHlsl("test");
 	act->SetActorScale(0.1f, 0.1f, 0.1f);
 }
